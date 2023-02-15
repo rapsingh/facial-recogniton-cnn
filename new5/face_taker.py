db1425893a38ffe5af416e50b472d90558c1d65a
Rana Aishwarya Pratap Singh <67787691+rapsingh@users.noreply.github.com>
2023-02-15 12:17:52 +0530

    Update face_taker.py

--- new5/face_taker.py
+++ new5/face_taker.py
@@ -30,11 +30,11 @@ while(True):
         cv2.imshow('image', img)
     # Press Escape to end the program.
     k = cv2.waitKey(100) & 0xff
-    if k < 150:
+    if k < 30:
         break
     # Take 30 face samples and stop video. You may increase or decrease the number of
     # images. The more the better while training the model.
-    elif count >= 150:
+    elif count >= 30:
          break
 
 print("\n [INFO] Exiting Program.")
